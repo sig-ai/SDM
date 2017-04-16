70a1eae27d15b5f95b8c93ad4fe075a9afeed2cc
Akshay Mishra <lck.akshay@gmail.com>
2017-04-16 02:57:54 -0500

    Added sensor, actuator, agent

--- sdm/abstract/core.py
+++ sdm/abstract/core.py
@@ -3,7 +3,7 @@ from __future__ import (absolute_import, division,
 from builtins import *
 import six
 from abc import ABCMeta, abstractmethod, abstractproperty
-
+import torch
 
 @six.add_metaclass(ABCMeta)
 class AbstractWorld:
@@ -30,8 +30,46 @@ class AbstractWorld:
     @abstractproperty
     def num_agents(self):
         pass
-    
+
+
+@six.add_metaclass(ABCMeta)
+class Sensor:
+
+    @abstractmethod
+    def observe(self):
+        pass
+
+    @abstractproperty
+    def space(self):
+        pass
+
+    def __call__(self):
+        return self.observe()
+
+
+@six.add_metaclass(ABCMeta)
+class Actuator:
+
+    @abstractmethod
+    def act(self):
+        pass
+
+    @abstractproperty
+    def space(self):
+        pass
+
+    def __call__(self):
+        self.act()
+
+
 
 @six.add_metaclass(ABCMeta)
 class AbstractAgent:
-    pass
+
+    def __init__(self, sensor, actuator, model):
+        in_shape = model.input_shape
+        out_shape = model(torch.zeros(1, *in_shape)).shape
+        if in_shape != sensor.space.shape():
+            raise ValueError
+        self.sensor = sensor
+        self.actuator = actuator
